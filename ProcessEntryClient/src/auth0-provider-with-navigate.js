1189d801f8415663f760e98b8cc23e18deb0a946
Patrick-Misner <misner.patrick@gmail.com>
2023-01-25 04:57:02 -0700

    nothing works

--- ProcessEntryClient/src/auth0-provider-with-navigate.js
+++ ProcessEntryClient/src/auth0-provider-with-navigate.js
@@ -8,6 +8,7 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
